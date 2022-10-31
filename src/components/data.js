55041bbb9ae790618690f04c20714535efe6450d
aselwilliams <aselwilliams22@gmail.com>
2022-10-31 08:40:23 -0500

    whatsapp project url replaced

--- src/components/data.js
+++ src/components/data.js
@@ -3,7 +3,7 @@ export const products = [
     id: 1,
     title: "WhatsApp Clone",
     img: "https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODR8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    link: "https://63214abb00132e08c28be783--whatsapp-version.netlify.app/",
+    link: "https://whatsapp-asel-version.netlify.app/",
     desc: "A messaging app that enables users to instant message and connect with each other through their computers or mobile devices. Tech stack: React.js, Firebase Firestore Realtime db, Material UI, React Router, Context API, Google Authentication.",
   },
   {
